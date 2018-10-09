a2850ba7c405cc54f2d387a379927addc54f13ef
鲍志强 <530420396@qq.com>
2018-10-09 19:38:33 +0800

    决策树

--- work/cluser_train-master (1)/cluser_train-master/test.py
+++
@@ -1,7 +0,0 @@
-
-temparray = [1,2,3]
-temparraylen = len(temparray)
-
-for x in range(temparraylen):
-    for y in range( x +1, temparraylen):
-        print(x,y)
---
+++ work/testTree/test10.py
@@ -0,0 +1,34 @@
+#-*- coding:utf-8 -*-
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+import xlrd
+from sklearn.tree import DecisionTreeRegressor
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split  # 用于划分训练集与测试集
+
+
+
+# 决策树， 训练
+
+data = pd.read_excel('test.xlsx')
+all_inputs = data
+all_classes = data['labels'].values
+# 划分训练集与测试集
+(training_inputs, testing_inputs, training_classes, testing_classes) = train_test_split(all_inputs, all_classes,
+                                                                                        test_size=0.2, random_state=1)
+# 建立决策树  在此全部为默认参数了  主要参数criterion可选‘gini'或'entropy'作为生成树依据,max_deoth可以决定树的深度，max_leaf_nodes限制最大叶子树
+decision_tree_classifier = DecisionTreeClassifier()
+decision_tree_classifier.fit(training_inputs, training_classes)
+
+decision_tree_output = decision_tree_classifier.predict(testing_inputs)
+print('真实值是：')
+print(testing_classes)
+
+print('预测值是:')
+print(decision_tree_output)
+
+
